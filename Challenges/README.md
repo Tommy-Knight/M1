3263dff633098bee85ff89112a82858a67bee31b
Tommy-Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-03-18 23:05:05 +0000

    Strive

---
+++ Challenges/README.md
@@ -0,0 +1,24 @@
+Chess Board [Easy]
+Render 8x8 chess board ( 64 div, which has 50px width and height ).
+
+In for loop make odd numbers orange even numbers brown;
+
+LCD Screen [Medium]
+Render 20x20 lcd screen ( 400 div, which has 30px width and height ).
+
+In while loop change their colors sequentially as soon as they are rendered.
+
+First make it red, after 500ms blue ,after 500ms green (use setTimeout)
+
+Minesweeper Game [Hard]
+Size of game board = n Number of mines = m User score is = mx10
+
+Generate nxn board with divs, add some random mine to m divs.
+
+(use data-mine="true") attribute. learn data-attributes here
+
+attach click event to every div.
+
+When user clicked on the div if there is mine put mine icon inside of div 
+
+and reduce user score 10 , if user score is 0 show replay button. If user can click m times without clicking to mines , user wins.
--- D9/script.js
+++ D9/script.js
@@ -1,20 +1,23 @@
 const bingoBoard = document.getElementById("bingoBoard")
-function createBoard(){
+const createBoard = ()=>{
     for(let i=0; i<76; i++) {
         const cell = document.createElement("div")
         cell.classList.add('cell')
         cell.innerText = i+ 1
         bingoBoard.appendChild(cell)
     }   
-}   
-button.onclick = () =>{
-
-    let button = document.getElementById("button")
-    let randomNum = Math.floor(Math.random() * 76) + 1
-    button.innerHTML = `Calling number ${randomNum}!`
-    bingoBoard.children[randomNum-1].classList.add("highlight") 
 }
-window.onload=function(){
+const checklist = []
+button.onclick = ()=>{
+    const button = document.getElementById("button")
+    let randomNum = Math.floor(Math.random() * 76) + 1
+    if (checklist.indexOf(randomNum) < 0 ){
+        checklist.push(randomNum)
+        button.innerHTML = `Calling number ${randomNum}!`
+        bingoBoard.children[randomNum-1].classList.add("highlight")
+    }
+}
+
+window.onload= ()=>{
     createBoard()
 }
-
